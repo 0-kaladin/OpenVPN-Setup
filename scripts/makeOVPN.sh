f434ca17f945c550e0d642cd5f852d960caa7327
Kaladin Light <0.kaladin@gmail.com>
2016-04-16 13:19:44 -0400

    Rework to make even easier to install by simply curl'ing one command

--- scripts/makeOVPN.sh
+++ scripts/makeOVPN.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
- 
+# Create OVPN Client 
 # Default Variable Declarations 
 DEFAULT="Default.txt" 
 FILEEXT=".ovpn" 
@@ -8,42 +8,31 @@ OKEY=".key"
 KEY=".3des.key" 
 CA="ca.crt" 
 TA="ta.key" 
-
-#Ask for a Client name
-#NAME=$(whiptail --inputbox "Please enter a Name for the Client:" \
-#8 78 --title "MakeOVPN" 3>&1 1>&2 2>&3)
-#exitstatus=$?
-#if [ $exitstatus = 0 ]; then
-# whiptail --title "MakeOVPN" --infobox "Name: $NAME" 8 78
-#else
-# whiptail --title "MakeOVPN" --infobox "Cancelled" 8 78
-# exit
-#fi
+INSTALL_USER=$(cat /etc/pivpn/INSTALL_USER)
 
 echo "Please enter a Name for the Client:"
 read NAME 
  
 #Build the client key and then encrypt the key
-sudo chmod 777 -R /etc/openvpn
 cd /etc/openvpn/easy-rsa
 source ./vars
 ./build-key-pass $NAME
 cd keys
 openssl rsa -in $NAME$OKEY -des3 -out $NAME$KEY
  
-#1st Verify that client�s Public Key Exists 
+#1st Verify that clients Public Key Exists 
 if [ ! -f $NAME$CRT ]; then 
  echo "[ERROR]: Client Public Key Certificate not found: $NAME$CRT" 
  exit 
 fi 
-echo "Client�s cert found: $NAME$CR" 
+echo "Client's cert found: $NAME$CRT" 
  
 #Then, verify that there is a private key for that client 
 if [ ! -f $NAME$KEY ]; then 
  echo "[ERROR]: Client 3des Private Key not found: $NAME$KEY" 
  exit 
 fi 
-echo "Client�s Private Key found: $NAME$KEY"
+echo "Client's Private Key found: $NAME$KEY"
  
 #Confirm the CA public key exists 
 if [ ! -f $CA ]; then 
@@ -59,7 +48,7 @@ if [ ! -f $TA ]; then
 fi 
 echo "tls-auth Private Key found: $TA" 
  
-#Ready to make a new .opvn file - Start by populating with the 
+#Ready to make a new .ovpn file - Start by populating with the 
 #default file 
 cat $DEFAULT > $NAME$FILEEXT 
  
@@ -84,10 +73,9 @@ cat $TA >> $NAME$FILEEXT
 echo "</tls-auth>" >> $NAME$FILEEXT 
 
 # Copy the .ovpn profile to the home directory for convenient remote access
-cp /etc/openvpn/easy-rsa/keys/$NAME$FILEEXT /home/pi/ovpns/$NAME$FILEEXT
-sudo chmod 600 -R /etc/openvpn
+cp /etc/openvpn/easy-rsa/keys/$NAME$FILEEXT /home/$INSTALL_USER/ovpns/$NAME$FILEEXT
 echo "$NAME$FILEEXT moved to home directory."
 whiptail --title "MakeOVPN" --msgbox "Done! $NAME$FILEEXT successfully created and \
-moved to directory /home/pi/ovpns." 8 78
+moved to directory /home/$INSTALL_USER/ovpns." 8 78
  
 # Original script written by Eric Jodoin.
